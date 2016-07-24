3c79bb183f6e77a368f5ea74cae5c4d93c8a4aa0
Chirag Tailor <ctailor2@gmail.com>
2016-07-23 22:27:26 -0500

    Add jest and enzyme for testing

---
+++ src/components/__tests__/App-test.js
@@ -0,0 +1,17 @@
+jest.unmock('../App');
+
+import App from '../App.js';
+import React from 'react';
+import {shallow} from 'enzyme';
+
+describe('App', () => {
+    let tree;
+
+    beforeEach(() => {
+        let tree = shallow(<App />);
+    });
+
+    it('renders', () => {
+        expect(tree.length).toBe(1);
+    });
+});
