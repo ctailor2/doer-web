d3e05d16c328ea31fca3d664ec7a4ccba2d2d9f1
Chirag Tailor <ctailor2@gmail.com>
2016-07-23 22:11:36 -0500

    Add redux and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
+import './styles/index.css';
 
-ReactDOM.render(
-  <App />,
+// Replace the noop function with rootReducer once a reducer is added
+let store = createStore(() => {});
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
