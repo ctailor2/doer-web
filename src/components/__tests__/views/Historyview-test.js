d40dccf790f9a0daeaae78fe40dbc2e0e0278796
Chirag Tailor <ctailor2@gmail.com>
2017-04-11 17:02:46 -0500

    Don't try loading the history view unless there is a session token present

--- src/components/__tests__/views/Historyview-test.js
+++ src/components/__tests__/views/Historyview-test.js
@@ -5,28 +5,38 @@ import React from 'react';
 import {HistoryView, mapStateToProps} from '../../views/HistoryView';
 import Header from '../../Header';
 import History from '../../History';
+import {browserHistory} from 'react-router';
 
 describe('HistoryView', () => {
-	let tree, mockLoadHistoryViewActionFn;
+    let tree, mockLoadHistoryViewActionFn;
 
-	beforeEach(() => {
-		mockLoadHistoryViewActionFn = jest.fn();
-	    localStorage.getItem = jest.fn(() => {return 'http://some.api/endpoint'});
-		tree = shallow(<HistoryView viewLoaded={false} loadHistoryViewAction={mockLoadHistoryViewActionFn} />,
-                       {lifecycleExperimental: true});
-	});
-
-    it('fires load history view action when mounted', () => {
-        expect(mockLoadHistoryViewActionFn).toBeCalled();
+    beforeEach(() => {
+        browserHistory.push = jest.fn();
+        mockLoadHistoryViewActionFn = jest.fn();
+        tree = shallow(<HistoryView viewLoaded={false} loadHistoryViewAction={mockLoadHistoryViewActionFn} />);
     });
 
-	it('renders', () => {
-		expect(tree.length).toBe(1);
-	});
+    it('redirects to the login page if a sessionToken is not present', () => {
+        localStorage.getItem = jest.fn(() => {return null});
+        tree = shallow(<HistoryView viewLoaded={false} loadHistoryViewAction={mockLoadHistoryViewActionFn} />, {lifecycleExperimental: true});
+        expect(browserHistory.push).toBeCalledWith('/login');
+        expect(mockLoadHistoryViewActionFn).not.toBeCalled();
+    });
 
-	it('has a header', () => {
-	    expect(tree.find(Header).length).toBe(1);
-	});
+    it('fires load history view action when mounted', () => {
+        localStorage.getItem = jest.fn(() => {return 'cooltoken'});
+        tree = shallow(<HistoryView viewLoaded={false} loadHistoryViewAction={mockLoadHistoryViewActionFn} />, {lifecycleExperimental: true});
+        expect(mockLoadHistoryViewActionFn).toBeCalled();
+        expect(browserHistory.push).not.toBeCalled();
+    });
+
+    it('renders', () => {
+        expect(tree.length).toBe(1);
+    });
+
+    it('has a header', () => {
+        expect(tree.find(Header).length).toBe(1);
+    });
 
     it('renders the loader when view is not loaded', () => {
         expect(tree.find('Loader').length).toBe(1);
@@ -37,10 +47,10 @@ describe('HistoryView', () => {
         expect(tree.find(History).length).toBe(1);
     });
 
-	it('maps state to props', () => {
-	    let state = {loadView: {historyViewLoaded: true}};
-	    expect(mapStateToProps(state)).toEqual({
-	        viewLoaded: true
-	    });
-	});
+    it('maps state to props', () => {
+        let state = {loadView: {historyViewLoaded: true}};
+        expect(mapStateToProps(state)).toEqual({
+            viewLoaded: true
+        });
+    });
 });
--- src/components/views/HistoryView.js
+++ src/components/views/HistoryView.js
@@ -4,33 +4,39 @@ import {loadHistoryViewAction} from '../../actions/loadViewActions';
 import Header from '../Header';
 import History from '../History';
 import Loader from '../views/Loader';
+import _ from 'lodash';
+import {browserHistory} from 'react-router';
 
 export class HistoryView extends Component {
-	componentDidMount() {
-		this.props.loadHistoryViewAction();
-	}
+    componentDidMount() {
+        if(_.isNull(localStorage.getItem('sessionToken'))) {
+            browserHistory.push('/login');
+        } else {
+            this.props.loadHistoryViewAction();
+        }
+    }
 
-	render() {
-		return (<div>
-			<Header />
-			{this.renderView()}
-		</div>);
-	}
+    render() {
+        return (<div>
+            <Header />
+            {this.renderView()}
+        </div>);
+    }
 
-	renderView() {
-		if(this.props.viewLoaded) {
-			return (<History />);
-		}
-		return (<Loader />)
-	}
+    renderView() {
+        if(this.props.viewLoaded) {
+            return (<History />);
+        }
+        return (<Loader />)
+    }
 }
 
 export const mapStateToProps = (state) => {
-	return {
-		viewLoaded: state.loadView.historyViewLoaded
-	};
+    return {
+        viewLoaded: state.loadView.historyViewLoaded
+    };
 }
 
 export default connect(mapStateToProps, {
-	loadHistoryViewAction
+    loadHistoryViewAction
 })(HistoryView);
