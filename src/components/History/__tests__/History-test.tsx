6e6b5cd524c5ce3c5d84894ba262ae86278f9848
Chirag Tailor <ctailor2@gmail.com>
2021-06-05 21:52:47 -0500

    Add react testing library and jest dom. Begin converting enzyme tests to react testing library.

--- src/components/History/__tests__/History-test.tsx
+++ src/components/History/__tests__/History-test.tsx
@@ -1,53 +1,30 @@
-import { configure, shallow, ShallowWrapper } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import {
-    ListGroupItem,
-    Panel,
-} from 'react-bootstrap';
 import History from '../History';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { CompletedTodo } from '../../../api/completedList';
 
 describe('History', () => {
-    let tree: ShallowWrapper;
-
-    beforeEach(() => {
-        configure({ adapter: new Adapter() });
-        tree = shallow(<History todos={[]} />);
-    });
-
-    it('renders', () => {
-        expect(tree.length).toBe(1);
-    });
-
     describe('when there are todos', () => {
+        const todos: CompletedTodo[] = [
+            { task: 'celebrate', completedAt: '2017-01-01T10:50:44+0000' },
+            { task: 'feel so good', completedAt: '2017-01-01T11:50:44+0000' },
+            { task: 'another day', completedAt: '2017-01-10T02:50:44+0000' },
+        ];
+
         beforeEach(() => {
-            tree.setProps({
-                todos: [
-                    { task: 'celebrate', completedAt: '2017-01-01T10:50:44+0000' },
-                    { task: 'feel so good', completedAt: '2017-01-01T11:50:44+0000' },
-                    { task: 'another day', completedAt: '2017-01-10T02:50:44+0000' },
-                ],
-            });
+            render(<History todos={todos}/>)
         });
 
-        it('renders a panel for each date', () => {
-            expect(tree.find(Panel).length).toBe(2);
+        it('groups todos by date', () => {
+            expect(screen.getByText('1/1/2017')).toBeVisible();
+            expect(screen.getByText('1/9/2017')).toBeVisible();
         });
 
-        describe('each panel', () => {
-            let panel: ShallowWrapper;
-
-            beforeEach(() => {
-                panel = tree.find(Panel).at(0);
-            });
-
-            it('displays the date as a header', () => {
-                expect(panel.find(Panel.Heading).children().text()).toEqual('1/1/2017');
-            });
-
-            it('renders each todo completed on the date', () => {
-                expect(panel.find(ListGroupItem).length).toBe(2);
-            });
+        it('renders each todo task', () => {
+            expect(screen.getByText(todos[0].task)).toBeVisible();
+            expect(screen.getByText(todos[1].task)).toBeVisible();
+            expect(screen.getByText(todos[2].task)).toBeVisible();
         });
     });
 });
